ab6618b83b4196decc30172c2cf02d04d19d5146
Oskari <104070288+oskuH@users.noreply.github.com>
2025-12-01 17:37:37 +0200

    Implement seat reservation and error handling

--- raft-node/console.py
+++ raft-node/console.py
@@ -2,64 +2,141 @@ from reservation_manager import ReservationManager
 import json
 
 def run_console(node: ReservationManager):
-    print('\n--- Welcome to the Interactive Console! ---\n')
+    print('\n--- Welcome to the Interactive Console! ---')
 
     while True:
-        print('Commands:\nadmin\nreserve-seat (not implemented)\ncancel-seat (not implemented)\nget-showtimes\nexit')
+        commandGlobal = '' # this is used to exit nested loops after 'cancel' or successful operation
+        print('\n--- COMMANDS ---\nadmin\nreserve-seat\ncancel-seat (not implemented)\nget-showtimes\nget-full-state\nexit\n')
+        print('Enter a command:')
         try:
             command = input().strip().lower()
 
             if command == 'admin':
                 while True:
-                    print('Admin Commands\nadd-showtime\nremove-showtime (not implemented)\nexit-admin')
+                    print('\n--- ADMIN COMMANDS ---\nadd-showtime\nremove-showtime (not implemented)\nreturn\nexit\n')
+                    print('Enter a command:')
                     command = input().strip().lower()
                     if command == 'add-showtime':
                         while True:
-                            print('Enter showtimeID (int | "cancel")')
+                            print('\nEnter showtimeID: (int | "cancel")')
                             adminInput = input().strip().lower()
-                            if adminInput.lower() == 'cancel':
+                            if adminInput == 'cancel':
                                 print('Operation canceled.')
                                 break
                             try:
                                 showtimeID = int(adminInput)
-                                print('Enter theater rows (list[dict[str, int]] | "cancel") (you can copy-paste from db.json)')
-                                adminInput = input().strip().lower()
-                                if adminInput.lower() == 'cancel':
-                                    print('Operation canceled.')
-                                    break
-                                try:
-                                    rows = json.loads(adminInput)
-                                    node.addShowtime(showtimeID, rows)
-                                    print('Showtime added successfully.')
-                                except json.JSONDecodeError:
-                                    print('Invalid input.')
+                                existingShowtimes = node.getShowtimes() 
+                                if showtimeID not in list(existingShowtimes): # this line simulates an application server in the sense that identical verification is also later executed by reservation_manager.py
+                                    while True:
+                                        print('\nEnter theater rows: (list[dict[str, int]] | "return" | "cancel") (you can copy-paste from db.json)')
+                                        adminInput = input().strip().lower()
+                                        if adminInput == 'cancel':
+                                            print('Operation canceled.')
+                                            commandGlobal = 'cancel'
+                                            break
+                                        elif adminInput == 'return':
+                                            break
+                                        try:
+                                            rows = json.loads(adminInput)
+                                            response = node.addShowtime(showtimeID, rows, sync=True)
+                                            print(f'\n{response["message"]}')
+                                            if response['success']:
+                                                commandGlobal = 'cancel' # to return to admin menu
+                                                break
+                                        except json.JSONDecodeError:
+                                            print('Invalid input.')
+                                        except Exception as e:
+                                            print(f'Error: {type(e).__name__}: {e}')
+                                else:
+                                    raise ValueError(f'\nERROR: Showtime with id {showtimeID} already exists.\n')
+                            except ValueError as e:
+                                print(f'Invalid input.{e}')
+                            if commandGlobal == 'cancel': # canceled after entering showtimeID --> return to admin menu
+                                commandGlobal = ''
                                 break
-                            except ValueError:
-                                print('Invalid input.')
 
                     if command == 'remove-showtime':
                         print(f'Oh no! This command is not implemented, yet.')
 
-                    if command == 'exit-admin':
+                    if command == 'return':
+                        break
+
+                    if command == 'exit':
+                        commandGlobal = 'exit'
                         break
 
 
             if command == "reserve-seat":
-                print(f'Oh no! This command is not implemented, yet.')
+                while True:
+                    print('\nWho is reserving? Enter userID: (int | "cancel")')
+                    userInput = input().strip().lower()
+                    if userInput == 'cancel':
+                        print('Operation canceled.')
+                        break
+                    try:
+                        userID = int(userInput)
+                        while True:
+                            availableShowtimes = node.getShowtimes()
+                            print(f'\nAvailable showtimes: {availableShowtimes}')
+                            print('Select showtime: (int | "return" | "cancel")')
+                            userInput = input().strip().lower()
+                            if userInput == 'cancel':
+                                print('Operation canceled.')
+                                commandGlobal = 'cancel'
+                                break
+                            elif userInput == 'return':
+                                break
+                            try:
+                                showtimeID = int(userInput)
+                                if showtimeID in availableShowtimes:
+                                    while True:
+                                        availableSeats = node.getAvailableSeats(showtimeID)
+                                        print(f'\nAvailable seats for showtime {showtimeID}: {availableSeats}')
+                                        print('Select seat: (str | "return" | "cancel")')
+                                        userInput = input().strip().lower()
+                                        if userInput == 'cancel':
+                                            print('Operation canceled.')
+                                            commandGlobal = 'cancel'
+                                            break
+                                        elif userInput == 'return':
+                                            break
+                                        try:      
+                                            response = node.reserveSeat(showtimeID, seatID=userInput, userID=userID, sync=True)
+                                            print(f'\n{response["message"]}')
+                                            if response['success']:
+                                                commandGlobal = 'cancel' # to return to user menu
+                                                break
+                                        except Exception as e:
+                                            print(f'Error: {type(e).__name__}: {e}')
+                                else:
+                                    print('Invalid input.')
+                            except ValueError:
+                                print('Invalid input.')
+                            if commandGlobal == 'cancel': # canceled after entering showtimeID --> return to user menu
+                                break # commandGlobal not edited because there is one more outer loop to exit
+                    except ValueError:
+                        print('Invalid input.')
+                    if commandGlobal == 'cancel': # canceled after entering userID --> return to user menu
+                        commandGlobal = ''
+                        break
             
-            if command == "cancel-seat":
+            if command == 'cancel-seat':
                 print(f'Oh no! This command is not implemented, yet.')
 
             elif command == 'get-showtimes':
-                value = node.getShowtimes()
-                print(value)
+                showTimes = node.getShowtimes()
+                print(showTimes)
 
-            elif command == 'exit':
+            elif command == 'get-full-state':
+                all = node.getFullState()
+                print(all)
+
+            elif command == 'exit' or commandGlobal == 'exit': # the latter activated when admin exits
                 print("Exiting.")
                 break
         
-            elif command == 'exit-admin':
-                # configure this inside the admin loop
+            elif command == 'return' or command == 'reserve-seat':
+                # catch exits from inner loops
                 continue
 
             elif command:
--- raft-node/reservation_manager.py
+++ raft-node/reservation_manager.py
@@ -1,36 +1,113 @@
 from pysyncobj import SyncObj, replicated
 from typing import TypedDict
-ActiveShowtimes = dict[int, dict[str, int]]
-# { showtimeID: { seatID: userID } }
+
+ActiveShowtimes = dict[int, dict[str, int]] # { showtimeID: { seatID: userID, seatID: userID ... } } 
+# available seats have userID = None
 TheaterRows = TypedDict('TheaterRows', {'row': str, 'seats': int})
+# used in addShowTime, something for Application Servers to mind
 
 class ReservationManager(SyncObj):
     def __init__(self, selfNodeAddr, otherNodeAddrs):
         super(ReservationManager, self).__init__(selfNodeAddr, otherNodeAddrs)
         self.__activeShowtimes: ActiveShowtimes = {}
 
-    @replicated
-    def addShowtime(self, showtimeID: int, rows: list[TheaterRows]):
-        if showtimeID not in self.__activeShowtimes:
-            self.__activeShowtimes[showtimeID] = {}
-
-        for row in rows:
-            for n in range(row['seats']):
-                self.__activeShowtimes[showtimeID][f'{row["row"]}{n+1}'] = None
-                
+    def getFullState(self):
         return self.__activeShowtimes
 
     @replicated
+    def addShowtime(self, showtimeID: int, rows: list[TheaterRows]):
+        if showtimeID in self.__activeShowtimes:
+            return {
+                'success': False,
+                'message': f'ERROR: Showtime with id {showtimeID} already exists.'
+            }
+    
+        self.__activeShowtimes[showtimeID] = {}
+
+        try:
+            for row in rows:
+                for n in range(row['seats']):
+                    self.__activeShowtimes[showtimeID][f'{row["row"]}{n+1}'] = None
+        except Exception as e:
+            del self.__activeShowtimes[showtimeID]
+            return {
+                'success': False,
+                'message': f'ERROR: {e}'
+            }
+        
+        return {
+            'success': True,
+            'message': f'Showtime {showtimeID} successfully added!'
+        }
+
+    @replicated
     def removeShowtime(self, showtimeID: int):
+        #TODO
         self.__activeShowtimes.pop(showtimeID)
         return self.__activeShowtimes
 
     def getShowtimes(self):
-        return self.__activeShowtimes
+        return list(self.__activeShowtimes.keys())
 
     @replicated
-    def reserveSeat(self, key: str, value: dict[str, int]):
+    def reserveSeat(self, showtimeID: int, seatID: str, userID: int):
+        if showtimeID not in self.__activeShowtimes:
+            return {
+                'success': False,
+                'message': f'ERROR: Showtime with ID {showtimeID} does not exist.',
+            }
+        
+        if seatID not in self.__activeShowtimes[showtimeID]:
+            return {
+                'success': False,
+                'message': f'ERROR: Seat with ID {seatID} does not exist in showtime {showtimeID}.',
+            }
+
+        if self.__activeShowtimes[showtimeID][seatID] is not None:
+            reserved_by = self.__activeShowtimes[showtimeID][seatID]
+            return {
+                'success': False,
+                'message': f'ERROR reserving for user {userID}: Seat {seatID} for show {showtimeID} is already booked for user {reserved_by}.',
+            }
+
+        self.__activeShowtimes[showtimeID][seatID] = userID
+
+        return {
+            'success': True,
+            'message': f'Seat {seatID} for showtime {showtimeID} successfully reserved for user {userID}!',
+        }
+
+    def getAvailableSeats(self, showtimeID: int):
+        if showtimeID not in self.__activeShowtimes:
+            return {
+                'success': False,
+                'message': f'ERROR: Showtime with ID {showtimeID} does not exist.'
+            }
+
+        try:
+            availableSeats = []
+            for seatID, userID in self.__activeShowtimes[showtimeID].items():
+                if userID == None:
+                    availableSeats.append(seatID)
+        except Exception as e:
+            return {
+                'success': False,
+                'message': f'ERROR: {e}'
+            }
+
+        return availableSeats
+    
+    def getAvailableShowtimes(self):
+        # TODO this should return showtimes that have at least one free seat
+        return
+    
+    def cancelSeat(self):
+        # TODO
         return
 
     def getSeats(self, showtimeID: int):
+        # TODO
         return
+    
+# DEV NOTE: you can copy-paste the line below (taken from db.json) when using addShowtime
+# [{"row": "A","seats": 2},{"row": "B","seats": 2}]
